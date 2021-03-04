0199b0010fc5ab835006ad50c902af2a81d85ebd
Carlos Rodrigues <mundofreakuc@gmail.com>
2021-03-04 05:40:11 +0000

    v0.1.22

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true
   },
   parser: 'babel-eslint',
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
