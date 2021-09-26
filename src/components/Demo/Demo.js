1a38beca440a07e9458afdc671ddaef2a6ed57b2
Carlos Rodrigues <eduardocque@gmail.com>
2021-09-26 03:18:19 +0100

    v0.1.94

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -3,40 +3,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { compose } from 'redux';
-import { withElement, BaseElement } from '@plitzi/plitzi-element';
+import { withElement } from '@plitzi/plitzi-element';
 
 // Styles
 import './Assets/index.scss';
 
-class Demo extends BaseElement {
-  render() {
-    const { className, internalProps, content } = this.props;
-    const { ref, style, previewMode } = internalProps;
+const Demo = props => {
+  const { className, internalProps, content } = props;
+  const { ref, style, previewMode } = internalProps;
 
-    if (!previewMode) {
-      return (
-        <div ref={ref} style={style} className={classNames('plitzi-component__demo', { [className]: className })}>
-          {content}
-        </div>
-      );
-    }
-
+  if (!previewMode) {
     return (
       <div ref={ref} style={style} className={classNames('plitzi-component__demo', { [className]: className })}>
-        Hi, this is a Plitzi demo component
+        {content}
       </div>
     );
   }
-}
+
+  return (
+    <div ref={ref} style={style} className={classNames('plitzi-component__demo', { [className]: className })}>
+      Hi, this is a Plitzi demo component
+    </div>
+  );
+};
 
 Demo.defaultProps = {
-  ...BaseElement.defaultProps,
+  internalProps: null,
   className: '',
   content: 'Demo Component'
 };
 
 Demo.propTypes = {
-  ...BaseElement.propTypes,
+  internalProps: PropTypes.object,
   className: PropTypes.string,
   content: PropTypes.string
 };
--- src/components/Demo/Settings.js
+++ src/components/Demo/Settings.js
@@ -1,26 +1,19 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { BaseSettings } from '@plitzi/plitzi-element';
 
-class Settings extends BaseSettings {
-  render() {
-    return (
-      <div className="element-settings__container">
-        <h1>LIST OPTIONS</h1>
-        <div className="settings-item">
-          <label>Label</label>
-        </div>
+const Settings = () => {
+  return (
+    <div className="element-settings__container">
+      <h1>LIST OPTIONS</h1>
+      <div className="settings-item">
+        <label>Label</label>
       </div>
-    );
-  }
-}
-
-Settings.defaultProps = {
-  ...BaseSettings.defaultProps
+    </div>
+  );
 };
 
-Settings.propTypes = {
-  ...BaseSettings.propTypes
-};
+Settings.defaultProps = {};
+
+Settings.propTypes = {};
 
 export default Settings;
