e2865d5655123907bc30d3a837fb142a1c2660f3
Carlos Rodriguez <carlos_eduardo309@hotmail.com>
2026-04-14 01:43:16 +0100

    changes

--- src/component/Settings.tsx
+++ src/component/Settings.tsx
@@ -1,5 +1,10 @@
-import Input from '@plitzi/plitzi-ui/Input';
+import Input, { inputTheme } from '@plitzi/plitzi-ui/Input';
+import Provider from '@plitzi/plitzi-ui/Provider';
 import { useCallback } from 'react';
+
+const components = {
+  Input: inputTheme
+};
 
 export type SettingsProps = {
   content?: string;
@@ -10,14 +15,16 @@ const Settings = ({ content = '', onUpdate }: SettingsProps) => {
   const handleChangeContent = useCallback((value: string) => onUpdate?.('content', value), [onUpdate]);
 
   return (
-    <div className="flex flex-col">
-      <div className="bg-[#1A2835] px-4 py-2 flex items-center justify-center">
-        <h1 className="text-white m-0">Demo Settings</h1>
+    <Provider components={components}>
+      <div className="flex flex-col">
+        <div className="bg-[#1A2835] px-4 py-2 flex items-center justify-center">
+          <h1 className="text-white m-0">Demo Settings</h1>
+        </div>
+        <div className="flex flex-col w-full px-4 py-2">
+          <Input id="content" label="Content" size="xs" value={content} onChange={handleChangeContent} />
+        </div>
       </div>
-      <div className="flex flex-col w-full px-4 py-2">
-        <Input id="content" label="Content" size="xs" value={content} onChange={handleChangeContent} />
-      </div>
-    </div>
+    </Provider>
   );
 };
 
